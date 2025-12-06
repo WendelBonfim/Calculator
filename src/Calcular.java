cbed72acab1e1920c04ab3a4412079e3d2be0439
unknown <wendel.bonfims@gmail.com>
2025-12-06 18:21:26 -0300

    first commit

---
+++ src/Calcular.java
@@ -0,0 +1,33 @@
+public class Calcular {
+
+
+
+    public Integer soma(String somaNumUm, String somaNumDois){
+        Integer somaUmInt = Integer.parseInt(somaNumUm);
+        Integer somaDoisInt = Integer.parseInt(somaNumDois);
+        Integer result = somaUmInt + somaDoisInt;
+
+        return result;
+    }
+
+    public String subtrair(Integer subNumeroUm, Integer subNumeroDois){
+        Integer resultSub = subNumeroUm - subNumeroDois;
+        String resultString = Integer.toString(resultSub);
+        return  resultString;
+    }
+
+    public String multiplicacao(Double multUm, Double multDois){
+        Double resultDoub = multUm * multDois;
+        String resultStringMult = Double.toString(resultDoub);
+        return  resultStringMult;
+    }
+    public Double divisao (String divUm, String divDois){
+        Double divUmDouble = Double.parseDouble(divUm);
+        Double divDoisDouble = Double.parseDouble(divDois);
+        Double resultDiv = divUmDouble / divDoisDouble;
+        return resultDiv;
+    }
+
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        Calcular calcular = new Calcular();
+
+       Integer resultSoma = calcular.soma("2","2");
+
+        System.out.println("O resultado da soma e: " + resultSoma);
+
+        String sub = calcular.subtrair(3 ,5);
+
+        System.out.println("A subtracao e: " + sub);
+
+        String mult = calcular.multiplicacao(10.8,15.4);
+
+        System.out.println("A multiplicacao e:" + mult);
+
+        Double resultDiv = calcular.divisao("10", "2");
+        System.out.println("A divisao é :" + resultDiv);
+    }
+}
