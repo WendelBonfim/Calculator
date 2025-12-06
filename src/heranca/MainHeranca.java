8d441f05eb72adfabd5e3635524e65482f439113
unknown <wendel.bonfims@gmail.com>
2025-12-06 20:10:45 -0300

    continuando a trabalhar com herança

--- src/heranca/MainHeranca.java
+++ src/heranca/MainHeranca.java
@@ -2,4 +2,10 @@ package heranca;
 
 public class MainHeranca {
 
+    public static void main(String[] args) {
+        Cachorro cachorro = new Cachorro("Pipo",10,"Pudlletoy");
+        System.out.println("O nome do cachorro e  "+ cachorro.nome + "  a idade dele e "+ cachorro.idade + " e a raca e " + cachorro.raca);
+
+    }
+
 }
