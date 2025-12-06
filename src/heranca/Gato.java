33caa1b5fef33e3b6d190aebd42e52331955c50f
unknown <wendel.bonfims@gmail.com>
2025-12-06 20:28:34 -0300

    finalizando herança

--- src/heranca/Gato.java
+++ src/heranca/Gato.java
@@ -1,4 +1,13 @@
 package heranca;
 
-public class Gato {
+public class Gato extends Animal{
+    public String raca;
+    public Sexo sexo;
+
+    public Gato(String nome, Integer idade, String raca, Sexo sexo) {
+        super(nome, idade);
+        this.raca = raca;
+        this.sexo = sexo;
+
+    }
 }
